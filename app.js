97cc29ea566b15dc0a52af5ba21deb8db44a43c7
Kaushal Bundel <kaushalbundel@gmail.com>
2025-04-15 14:47:28 +0530

    Added app.js

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const app = createApp({
+const app = Vue.createApp({
   data() {
-    //data
+    return {
+      currentView: "techniques-list",
+      selectedTechnique: null,
+      availableTechnique: [
+        {
+          id: "478",
+          name: "4-7-8 Breathing",
+          description:
+            "Breathe in for 4 secs, Hold Breath for 7 secs, exhale for 8 secs. Helps reduce anxiety.",
+        },
+        {
+          id: "box",
+          name: "Box Breathing",
+          description:
+            "Equal counts of 4 for inhale, hold, exhale and pause. Builds mental focus.",
+        },
+      ],
+    };
   },
   methods: {
-    // methods
+    selectedTechnique(techniqueId) {
+      (this.selectedTechnique = techniqueId),
+        (this.currentView = "timer-setup");
+    },
+    goToTechniquesList() {
+      this.currentView = "techniques-list";
+    },
   },
   computed: {
     // computed data
   },
 });
+
+// App mount
+app.mount("#app");
